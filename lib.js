c2da58f1543f02f4ebf8fe9eadfbe041f0287a92
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-04 22:24:28 +0800

    lib.js, og version

---
+++ lib.js
@@ -0,0 +1,24 @@
+/// SQUARE OF N
+const square = (n) => {
+    let sq = Math.pow(n, 2);
+    return sq;
+}
+/// F TO C
+const farenheit = (celsius) => {
+    let conv = celsius * 1.8 + 32;
+    return conv;
+}
+
+/// AREA OF A RECTANGLE
+const rectangleArea = (a,b) => {
+    let area = a * b;
+    return area;
+}
+
+/// PALINDROME
+const isPalindrome = (str) => {
+    let lowStr = str.toLowerCase();
+    let start = lowStr;
+    let end = lowStr.toLowerCase().split('').reverse().join('');
+    return end == start;
+}
