e0cac09cbc8d55b1053635b04bbc5dc82541edae
Leila Jorg Barbosa <113877946+leibrbs7@users.noreply.github.com>
2022-10-05 09:45:43 +0800

    String prototype activity - leila (#1)
    Functions for square of a number, area of rec, conversion from f to c, palindrome using string prototype

---
+++ lib.js
@@ -0,0 +1,11 @@
+const square = (n) => Math.pow(n, 2);
+
+const farenheit = (celsius) => celsius * 1.8 + 32;
+
+const rectangleArea = (a, b) => a * b;
+
+const isPalindrome = (str) => {
+    let lowStr = str.toLowerCase();
+    let reverse = lowStr.split('').reverse().join('');
+    return lowStr === reverse;
+}
