520a737fe3d5215a356ad9edd68cdf5995cb5b5c
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-05 10:56:03 +0800

    removed extra whitespace, put space after comma

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const { square, farenheit, rectangleArea, isPalindrome } = require('./lib');
 
 console.log(square(2));
 console.log(farenheit(2));
-console.log(rectangleArea(3,4));
+console.log(rectangleArea(3, 4));
 console.log(isPalindrome('Racecar'));
 console.log(isPalindrome('Dog'));
