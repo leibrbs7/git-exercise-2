6b2ac65fa741af000900508920305359a5aec057
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-05 08:54:24 +0800

    modified all functions

--- lib.js
+++ lib.js
@@ -1,24 +1,16 @@
 /// SQUARE OF N
-const square = (n) => {
-    let sq = Math.pow(n, 2);
-    return sq;
-}
+const square = (n) => sq = Math.pow(n, 2);
+
 /// F TO C
-const farenheit = (celsius) => {
-    let conv = celsius * 1.8 + 32;
-    return conv;
-}
+const farenheit = (celsius) => conv = celsius * 1.8 + 32;
 
 /// AREA OF A RECTANGLE
-const rectangleArea = (a,b) => {
-    let area = a * b;
-    return area;
-}
+const rectangleArea = (a,b) => area = a * b;
 
 /// PALINDROME
 const isPalindrome = (str) => {
     let lowStr = str.toLowerCase();
     let start = lowStr;
     let end = lowStr.split('').reverse().join('');
-    return end == start;
+    return end === start;
 }
