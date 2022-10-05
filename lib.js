48b0b57a7297830d564f18df7f5c371e5082e198
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-05 09:23:15 +0800

    added spaces, removed unnecessary comments

--- lib.js
+++ lib.js
@@ -1,13 +1,9 @@
-/// SQUARE OF N
 const square = (n) => sq = Math.pow(n, 2);
 
-/// F TO C
 const farenheit = (celsius) => conv = celsius * 1.8 + 32;
 
-/// AREA OF A RECTANGLE
-const rectangleArea = (a,b) => area = a * b;
+const rectangleArea = (a, b) => area = a * b;
 
-/// PALINDROME
 const isPalindrome = (str) => {
     let lowStr = str.toLowerCase();
     let start = lowStr;
