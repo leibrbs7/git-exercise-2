0e93466966438081215b033c55683e3e7bd3d1eb
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-05 09:31:11 +0800

    last function fixed

--- lib.js
+++ lib.js
@@ -1,12 +1,11 @@
-const square = (n) => sq = Math.pow(n, 2);
+const square = (n) => Math.pow(n, 2);
 
-const farenheit = (celsius) => conv = celsius * 1.8 + 32;
+const farenheit = (celsius) => celsius * 1.8 + 32;
 
-const rectangleArea = (a, b) => area = a * b;
+const rectangleArea = (a, b) => a * b;
 
 const isPalindrome = (str) => {
     let lowStr = str.toLowerCase();
-    let start = lowStr;
-    let end = lowStr.split('').reverse().join('');
-    return end === start;
+    let reverse = lowStr.split('').reverse().join('');
+    return lowStr === reverse;
 }
