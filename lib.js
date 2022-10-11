838500a6cd17aaf1a22162c1c5c089cdc4bdf207
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-11 09:19:54 +0800

    added prettierrc and formatted

--- lib.js
+++ lib.js
@@ -5,14 +5,14 @@ const farenheit = (celsius) => celsius * 1.8 + 32;
 const rectangleArea = (a, b) => a * b;
 
 const isPalindrome = (str) => {
-    let lowStr = str.toLowerCase();
-    let reverse = lowStr.split('').reverse().join('');
-    return lowStr === reverse;
-}
+  let lowStr = str.toLowerCase();
+  let reverse = lowStr.split('').reverse().join('');
+  return lowStr === reverse;
+};
 
 module.exports = {
-    square,
-    farenheit,
-    rectangleArea,
-    isPalindrome
-}
+  square,
+  farenheit,
+  rectangleArea,
+  isPalindrome,
+};
